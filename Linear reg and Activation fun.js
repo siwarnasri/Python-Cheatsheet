c2b3a5b2a67f5b17ddc1aac57eacfa79544a84c6
NASRI Siwar <44530468+siwarnasri@users.noreply.github.com>
2022-10-16 03:47:58 +0100

    Create Linear reg and Activation fun.js

---
+++ Linear reg and Activation fun.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2 id="title1">Linear regression:</h2>
+
+
+<p id="image1"></p>
+
+<script>
+function display_image(src, width, height, alt) {
+    var a = document.createElement("image1");
+    a.src = src;
+    a.width = width;
+    a.height = height;
+    a.alt = alt;
+    document.body.appendChild(a);
+}
+display_image('https://github.com/siwarnasri/Python-Cheatsheet/blob/main/images/Sup/linear%20regression%20assumptions.webp', 
+                 500, 
+                 500, 
+                 'JavaScriptImage');
+</script>
+
+
+
+<h2 id="title1">Activation functions:</h2>
+
+<p id="image2"></p>
+
+<script>
+function display_image(src, width, height, alt) {
+    var a = document.createElement("image2");
+    a.src = src;
+    a.width = width;
+    a.height = height;
+    a.alt = alt;
+    document.body.appendChild(a);
+}
+display_image('https://github.com/siwarnasri/Python-Cheatsheet/blob/main/images/Sup/activation%20functions.webp', 
+                 500, 
+                 500, 
+                 'JavaScriptImage');
+</script>
+
+<script>
+document.getElementById("title1").style.color = "blue";
+document.getElementById("title1").style.fontFamily = "Arial";
+document.getElementById("title1").style.fontSize = "larger";
+</script>
+
+
+</body>
+</html>
